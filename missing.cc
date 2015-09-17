3359b461d802c021539241ba780ecc78f4fb7e66
Sebastian <sebastian.krahmer@gmail.com>
2015-09-17 14:02:19 +0200

    - opmsg: re-implementing missing BoringSSL functions to no longer need openssl copyright disclaimer

--- missing.cc
+++ missing.cc
@@ -1,62 +1,25 @@
-/* Mostly consists of modified code to implement missing functions for certain SSL
- * implementations, e.g. BoringSSL. Original Copyright header follows:
+/*
+ * This file is part of the opmsg crypto message framework.
+ *
+ * (C) 2015 by Sebastian Krahmer,
+ *             sebastian [dot] krahmer [at] gmail [dot] com
+ *
+ * opmsg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * opmsg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with opmsg.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* ====================================================================
- * Copyright (c) 1998-2002 The OpenSSL Project.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. All advertising materials mentioning features or use of this
- *    software must display the following acknowledgment:
- *    "This product includes software developed by the OpenSSL Project
- *    for use in the OpenSSL Toolkit. (http://www.openssl.org/)"
- *
- * 4. The names "OpenSSL Toolkit" and "OpenSSL Project" must not be used to
- *    endorse or promote products derived from this software without
- *    prior written permission. For written permission, please contact
- *    openssl-core@openssl.org.
- *
- * 5. Products derived from this software may not be called "OpenSSL"
- *    nor may "OpenSSL" appear in their names without prior written
- *    permission of the OpenSSL Project.
- *
- * 6. Redistributions of any form whatsoever must retain the following
- *    acknowledgment:
- *    "This product includes software developed by the OpenSSL Project
- *    for use in the OpenSSL Toolkit (http://www.openssl.org/)"
- *
- * THIS SOFTWARE IS PROVIDED BY THE OpenSSL PROJECT ``AS IS'' AND ANY
- * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OpenSSL PROJECT OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
- *
- * This product includes cryptographic software written by Eric Young
- * (eay@cryptsoft.com).  This product includes software written by Tim
- * Hudson (tjh@cryptsoft.com).
- *
- */
+#include <memory>
 
-#include <stdlib.h>
 extern "C" {
 #include <openssl/crypto.h>
 #include <openssl/evp.h>
@@ -64,64 +27,57 @@ extern "C" {
 #include <openssl/ec.h>
 }
 
+#include <iostream>
 namespace opmsg {
 
+using namespace std;
+
 #ifdef HAVE_BORINGSSL
-BIGNUM *EC_POINT_point2bn(const EC_GROUP * group, const EC_POINT * point, point_conversion_form_t form, BIGNUM * ret, BN_CTX * ctx)
+BIGNUM *EC_POINT_point2bn(const EC_GROUP *grp, const EC_POINT *pnt, point_conversion_form_t form, BIGNUM *ret, BN_CTX *ctx)
 {
-	size_t buf_len = 0;
-	unsigned char *buf;
+	size_t blen = 0;
 
-	buf_len = EC_POINT_point2oct(group, point, form, nullptr, 0, ctx);
-	if (buf_len == 0)
+	if ((blen = EC_POINT_point2oct(grp, pnt, form, nullptr, 0, ctx)) == 0)
 		return nullptr;
 
-	if ((buf = (unsigned char *)malloc(buf_len)) == nullptr)
+	unique_ptr<unsigned char[]> buf(new (nothrow) unsigned char[blen]);
+	if (!buf.get())
 		return nullptr;
 
-	if (!EC_POINT_point2oct(group, point, form, buf, buf_len, ctx)) {
-		free(buf);
+	if (!EC_POINT_point2oct(grp, pnt, form, buf.get(), blen, ctx))
 		return nullptr;
-	}
-	ret = BN_bin2bn(buf, buf_len, ret);
-
-	free(buf);
+	ret = BN_bin2bn(buf.get(), blen, ret);
 
 	return ret;
 }
 
 
-EC_POINT *EC_POINT_bn2point(const EC_GROUP * group, const BIGNUM * bn, EC_POINT * point, BN_CTX * ctx)
+EC_POINT *EC_POINT_bn2point(const EC_GROUP *grp, const BIGNUM *bn, EC_POINT *pnt, BN_CTX *ctx)
 {
-	size_t buf_len = 0;
-	unsigned char *buf;
-	EC_POINT *ret;
+	size_t blen = 0;
+	EC_POINT *ret = nullptr;
 
-	if ((buf_len = BN_num_bytes(bn)) == 0)
+	if ((blen = BN_num_bytes(bn)) == 0)
 		return nullptr;
-	buf = (unsigned char *)malloc(buf_len);
-	if (buf == nullptr)
+	unique_ptr<unsigned char[]> buf(new (nothrow) unsigned char[blen]);
+	if (!buf.get())
 		return nullptr;
 
-	if (!BN_bn2bin(bn, buf)) {
-		free(buf);
+	if (!BN_bn2bin(bn, buf.get()))
 		return nullptr;
-	}
-	if (point == nullptr) {
-		if ((ret = EC_POINT_new(group)) == nullptr) {
-			free(buf);
+
+	if (!pnt) {
+		if (!(ret = EC_POINT_new(grp)))
 			return nullptr;
-		}
 	} else
-		ret = point;
+		ret = pnt;
 
-	if (!EC_POINT_oct2point(group, ret, buf, buf_len, ctx)) {
-		if (point == nullptr)
+	if (EC_POINT_oct2point(grp, ret, buf.get(), blen, ctx) != 1) {
+		if (!pnt)
 			EC_POINT_clear_free(ret);
-		free(buf);
 		return nullptr;
 	}
-	free(buf);
+
 	return ret;
 }
 
--- missing.h
+++ missing.h
@@ -1,59 +1,21 @@
-/* Mostly consists of modified code to implement missing functions for certain SSL
- * implementations, e.g. BoringSSL. Original Copyright header follows:
- */
-
-/* ====================================================================
- * Copyright (c) 1998-2002 The OpenSSL Project.  All rights reserved.
+/*
+ * This file is part of the opmsg crypto message framework.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * (C) 2015 by Sebastian Krahmer,
+ *             sebastian [dot] krahmer [at] gmail [dot] com
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ * opmsg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
+ * opmsg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * 3. All advertising materials mentioning features or use of this
- *    software must display the following acknowledgment:
- *    "This product includes software developed by the OpenSSL Project
- *    for use in the OpenSSL Toolkit. (http://www.openssl.org/)"
- *
- * 4. The names "OpenSSL Toolkit" and "OpenSSL Project" must not be used to
- *    endorse or promote products derived from this software without
- *    prior written permission. For written permission, please contact
- *    openssl-core@openssl.org.
- *
- * 5. Products derived from this software may not be called "OpenSSL"
- *    nor may "OpenSSL" appear in their names without prior written
- *    permission of the OpenSSL Project.
- *
- * 6. Redistributions of any form whatsoever must retain the following
- *    acknowledgment:
- *    "This product includes software developed by the OpenSSL Project
- *    for use in the OpenSSL Toolkit (http://www.openssl.org/)"
- *
- * THIS SOFTWARE IS PROVIDED BY THE OpenSSL PROJECT ``AS IS'' AND ANY
- * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OpenSSL PROJECT OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
- *
- * This product includes cryptographic software written by Eric Young
- * (eay@cryptsoft.com).  This product includes software written by Tim
- * Hudson (tjh@cryptsoft.com).
- *
+ * You should have received a copy of the GNU General Public License
+ * along with opmsg.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef opmsg_missing_h
@@ -70,9 +32,9 @@ extern "C" {
 
 namespace opmsg {
 
-BIGNUM *EC_POINT_point2bn(const EC_GROUP * group, const EC_POINT * point, point_conversion_form_t form, BIGNUM * ret, BN_CTX * ctx);
+BIGNUM *EC_POINT_point2bn(const EC_GROUP *, const EC_POINT *, point_conversion_form_t, BIGNUM *, BN_CTX *);
 
-EC_POINT *EC_POINT_bn2point(const EC_GROUP * group, const BIGNUM * bn, EC_POINT * point, BN_CTX * ctx);
+EC_POINT *EC_POINT_bn2point(const EC_GROUP *, const BIGNUM *, EC_POINT *, BN_CTX *);
 
 }
 
