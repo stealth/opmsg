a3cc649c45ef30ab359c7175f2cb9404deddd4f6
Sebastian <sebastian.krahmer@gmail.com>
2015-09-17 14:07:56 +0200

    - opmsg: new version 1.61

--- opmsg.cc
+++ opmsg.cc
@@ -74,7 +74,7 @@ enum {
 };
 
 
-const string banner = "\nopmsg: version=1.60 -- (C) 2015 opmsg-team: https://github.com/stealth/opmsg\n\n";
+const string banner = "\nopmsg: version=1.61 -- (C) 2015 opmsg-team: https://github.com/stealth/opmsg\n\n";
 
 /* The iostream lib works not very well wrt customized buffering and flushing
  * (unlike C's setbuffer), so we use string streams and flush ourself when we need to.
